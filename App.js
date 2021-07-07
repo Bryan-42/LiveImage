1cba1ecc146a7cdb8aec360a0323ccc0614c3b0e
Bryan-42 <68830449+Bryan-42@users.noreply.github.com>
2021-07-07 11:36:01 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import PickImage from './Screens/Camera';
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <PickImage
+        
+      />
+    )
+  }
+}
